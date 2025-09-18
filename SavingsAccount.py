557e0d5846bb5108a1a01aa6899658b91531791f
Robert Figueroa <rfiguer3@charlotte.edu>
2025-09-18 13:50:24 -0400

    feat: SavingsAccount.py __init__ and interest_tick methods

--- SavingsAccount.py
+++ SavingsAccount.py
@@ -1,3 +1,11 @@
 import BankAccount
+
+
 class SavingsAccount(BankAccount):
-    pass
+    def __init__(self, customer_name, current, minimum, interest, routing_num, account_num):
+        super().__init__(customer_name, current, minimum, routing_num, account_num)
+        self.interest = interest
+
+    def interest_tick(self):
+        self.current_balance *= (1 + self.interest)
+    
