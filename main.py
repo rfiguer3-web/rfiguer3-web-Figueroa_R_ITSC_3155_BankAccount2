91155fed2b16f811eb78b7850cfaba6b32111de2
Robert Figueroa <rfiguer3@charlotte.edu>
2025-09-18 14:01:02 -0400

    fix: Fixed conventions for import statements

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import CheckingAccount as ca
-import SavingsAccount as sa
+import CheckingAccount
+import SavingsAccount
 
 # Creation of 2 savings accounts
-savings1 = sa.SavingsAccount("Robert", 100, 10, .05, 1234, 10001)
-savings2 = sa.SavingsAccount("Gabby", 150, 20, .04, 5678, 10002)
+savings1 = SavingsAccount.SavingsAccount("Robert", 100, 10, .05, 1234, 10001)
+savings2 = SavingsAccount.SavingsAccount("Gabby", 150, 20, .04, 5678, 10002)
 
 # Creation of 2 checking accounts
-checking1 = ca.CheckingAccount("Robert", 50, 10, 40, 1278, 10003)
-checking2 = ca.CheckingAccount("Gabby", 80, 12, 50, 3456, 10004)
+checking1 = CheckingAccount.CheckingAccount("Robert", 50, 10, 40, 1278, 10003)
+checking2 = CheckingAccount.CheckingAccount("Gabby", 80, 12, 50, 3456, 10004)
