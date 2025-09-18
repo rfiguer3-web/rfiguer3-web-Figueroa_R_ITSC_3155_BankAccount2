08db94cf3dbe118914908f6afaf270d5c7bf2644
Robert Figueroa <rfiguer3@charlotte.edu>
2025-09-18 13:56:51 -0400

    feat: main.py with 2 checking and 2 savings accounts

---
+++ main.py
@@ -0,0 +1,10 @@
+import CheckingAccount
+import SavingsAccount
+
+# customer_name, current, minimum, transfer_limit, routing_num, account_num
+
+savings1 = SavingsAccount("Robert", 100, 10, .05, 1234, 10001)
+savings2 = SavingsAccount("Gabby", 150, 20, .04, 5678, 10002)
+
+checking1 = CheckingAccount("Robert", 50, 10, 40, 1278, 10003)
+checking2 = CheckingAccount("Gabby", 80, 12, 50, 3456, 10004)
