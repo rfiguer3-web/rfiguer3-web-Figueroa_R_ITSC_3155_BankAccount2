5d4b2fc74d78820b0508d69cd87c0cdd3ddea551
Robert Figueroa <rfiguer3@charlotte.edu>
2025-09-18 13:06:49 -0400

    feat: BankAccount.py routing/account number fields

--- BankAccount.py
+++ BankAccount.py
@@ -11,10 +11,16 @@ class BankAccount:
     name = "Happy Bank"
 
     # instantiation
-    def __init__(self, customer_name, current, minimum):
+    def __init__(self, customer_name, current, minimum, routing_num, account_num):
         self.customer_name = customer_name
         self.current_balance = current
         self.minimum_balance = minimum
+        self._routing_number = routing_num
+        self.__account_number = account_num
+
+    # Protected getter for routing number
+    def _get_routing_number(self):
+        return self._routing_number
 
     # Deposit method
     def deposit(self, amount):
